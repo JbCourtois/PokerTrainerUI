9b31e79e385611c182d80356caefb7e713d0be7f
J.B. Courtois <jb.courtois.dev@gmail.com>
2024-02-18 19:42:37 +0100

    Filter OOP

--- src/apps/trainer/views.py
+++ src/apps/trainer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.http import Http404
 
 from .models import Spot, Hand
 
@@ -9,13 +10,20 @@ class SpotListView(ListView):
 
 
 def play_spot(request, spot_id):
-    hand = Hand.objects\
+    qs = Hand.objects\
         .filter(spot_id=spot_id)\
         .order_by('?')\
-        .select_related('spot')\
-        .first()
+        .select_related('spot')
+    if (hero_is_oop := request.GET.get("hero_is_oop")):
+        qs = qs.filter(hero_is_oop=hero_is_oop == '1')
+
+    hand = qs.first()
+    if hand is None:
+        raise Http404("No hand found for this spot")
+
     context = {
         'spot': hand.spot,
         'hand': hand,
     }
+    print(request.GET.get("hero_is_oop"))
     return render(request, 'trainer/play_spot.html', context)
